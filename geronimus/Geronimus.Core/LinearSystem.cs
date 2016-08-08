164ab549decff726a396c3980d5c7d197585dfa0
ceres r <ceres r>
2016-08-07 22:09:13 -0300

    :rocket: Adiciona propriedades do LinearSystem

--- geronimus/Geronimus.Core/LinearSystem.cs
+++ geronimus/Geronimus.Core/LinearSystem.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Geronimus.Core
 {
     public class LinearSystem
     {
+        public List<LinearEquation> Equations { get; private set; }
+
         public LinearSystem()
         {
-            throw new NotImplementedException();
+            this.Equations = new List<LinearEquation>(3);
+        }
+
+        public LinearSystem Add(LinearEquation equation)
+        {
+            this.Equations.Add(equation);
+            return this;
         }
     }
 }
