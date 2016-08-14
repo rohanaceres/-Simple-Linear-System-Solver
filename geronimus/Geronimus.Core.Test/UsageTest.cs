df36f0400098bb8896e22abdc6bf1b5c218e839e
ceres r <ceres r>
2016-08-14 12:43:33 -0300

    :fire: Retira os builders

--- geronimus/Geronimus.Core.Test/UsageTest.cs
+++ geronimus/Geronimus.Core.Test/UsageTest.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Geronimus.Core.Model;
-using Geronimus.Core.Builder;
 using System.Diagnostics;
+using Geronimus.Core.Methods;
 
 namespace Geronimus.Core.Test
 {
@@ -12,19 +11,17 @@ namespace Geronimus.Core.Test
         //[TestMethod]
         public void Jacobi1()
         {
-            LinearSystemResult result = new JacobiMethodBuilder()
-                .Method
-                .AddEquation(new LinearEquation(70,  1,  0, 363))
-                .AddEquation(new LinearEquation(60, -1,  1, 518))
-                .AddEquation(new LinearEquation(40,  0, -1, 307))
+            LinearSystemResult result = new JacobiMethod()
+                .AddEquation(new LinearEquation(70, 1, 0, 363))
+                .AddEquation(new LinearEquation(60, -1, 1, 518))
+                .AddEquation(new LinearEquation(40, 0, -1, 307))
                 .AddErrorRate(0.0001)
                 .SolveIt();
         }
         //[TestMethod]
         public void Jacobi2()
         {
-            LinearSystemResult result = new JacobiMethodBuilder()
-                .Method
+            LinearSystemResult result = new JacobiMethod()
                 .AddEquation(new LinearEquation(10, 2, -1, 7))
                 .AddEquation(new LinearEquation(1, 8, 3, -4))
                 .AddEquation(new LinearEquation(-2, -1, 10, 9))
--- geronimus/Geronimus.Core/Builder/JacobiMethodBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using Geronimus.Core.Methods;
-
-namespace Geronimus.Core.Builder
-{
-    /// <summary>
-    /// Builder of <see cref="JacobiMethod"/>.
-    /// </summary>
-    public class JacobiMethodBuilder : AbstractMethodBuilder<JacobiMethod> { }
-}
